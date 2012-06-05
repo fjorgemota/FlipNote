dc7afa54873683e298209449c42faf494411ee23
Najuan <na.juan@hotmail.com>
2012-06-05 17:14:52 -0300

    insercao de cadastrar materia

--- build/web/js/index.js
+++ build/web/js/index.js
@@ -1,10 +1,12 @@
-window.calendar = Calendar.setup({
-    cont:"calendar",
-    weekNumbers:false,
-    selectionType: Calendar.SEL_SINGLE,
-    min:Calendar.dateToInt(new Date(new Date().getTime()-86400)),
-    onSelect:function(e){
-        var d = Calendar.intToDate(this.selection.getFirstDate());
-        window.location.href = "#resumo/"+d.getFullYear()+"/"+(d.getMonth()+1)+"/"+d.getDate();
-    }
-});
+if(!!document.getElementById("calendar")){
+    window.calendar = Calendar.setup({
+        cont:"calendar",
+        weekNumbers:false,
+        selectionType: Calendar.SEL_SINGLE,
+        min:Calendar.dateToInt(new Date(new Date().getTime()-86400)),
+        onSelect:function(e){
+            var d = Calendar.intToDate(this.selection.getFirstDate());
+            window.location.href = "#resumo/"+d.getFullYear()+"/"+(d.getMonth()+1)+"/"+d.getDate();
+        }
+    });
+}
