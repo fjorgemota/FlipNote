57225d34c1861398a005074878d31fc3f91bd9db
Fernando Jorge Mota <f.j.mota13@gmail.com>
2012-06-06 15:36:54 -0300

    Implementacao de classe AR para tarefas basicas (evitando repeticao de codigo dentre duas classes)

--- src/java/modelo/TarefaBaseAR.java
+++ src/java/modelo/TarefaBaseAR.java
@@ -5,6 +5,9 @@
 package modelo;
 
 import java.util.Date;
+import util.ConjuntoResultados;
+import util.Data;
+import util.MySQL;
 
 /**
  *
@@ -34,7 +37,9 @@ public abstract class TarefaBaseAR implements AR {
     public void setData(Date data) {
         this.data = data;
     }
-
+    public void setData(String data) {
+        this.data = Data.getDate(data);
+    }
     public String getDescricao() {
         return descricao;
     }
@@ -43,11 +48,11 @@ public abstract class TarefaBaseAR implements AR {
         this.descricao = descricao;
     }
 
-    public int getId() {
+    public int getID() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setID(int id) {
         this.id = id;
     }
 
@@ -58,7 +63,11 @@ public abstract class TarefaBaseAR implements AR {
     public void setMateria(MateriaAR materia) {
         this.materia = materia;
     }
-
+    public void setMateria(int materia) {
+        this.materia = new MateriaAR();
+        this.materia.setID(materia);
+        this.materia.load();
+    }
     public float getNota() {
         return nota;
     }
@@ -74,19 +83,54 @@ public abstract class TarefaBaseAR implements AR {
     public void setNotificacao(Date notificacao) {
         this.notificacao = notificacao;
     }
+    public void setNotificacao(String notificacao) {
+        this.notificacao = Data.getDateTime(notificacao);
+    }
     protected boolean carrega(String nomeTabela) {
-        return true;
+        MySQL cliente = MySQL.getInstance();
+        String sql = "SELECT * FROM "+nomeTabela+" WHERE id='"+this.getID()+"';";
+        ConjuntoResultados linha = cliente.executaSelect(sql);
+        if(linha.next()){
+            this.setMateria(linha.getInt("materia"));
+            this.setData(linha.getString("data"));
+            this.setDescricao(linha.getString("descricao"));
+            this.setNotificacao(linha.getString("data"));
+            this.setNota(linha.getFloat("nota"));
+            return true;
+        }
+        return false;
     }
 
     protected boolean insere(String nomeTabela) {
-        return true;
+        MySQL cliente = MySQL.getInstance();
+        String sql = "INSERT INTO "+nomeTabela+"(id, materia, data, descricao, notificacao, nota, aluno) "
+                + "VALUES("+this.getID()+",'"+this.getMateriaSQL()+"', "
+                + "'"+Data.getDate(this.getData())+"', '"+this.getDescricao()+"', '"+Data.getDateTime(this.getNotificacao())+"', "+this.getNota()+", "+this.getCriadorSQL()+");";
+        return cliente.executaInsert(sql);
     }
-
     protected boolean atualiza(String nomeTabela) {
-        return true;
+        MySQL cliente = MySQL.getInstance();
+        String sql = "UPDATE "+nomeTabela+" SET materia="+this.getMateriaSQL()+", "
+                + "data='"+Data.getDate(this.getData())+"', descricao='"+this.getDescricao()+"',"
+                + "notificacao='"+Data.getDateTime(this.getNotificacao())+"', nota="+this.getNota()+", aluno="+this.getCriadorSQL()+" WHERE id="+this.getID()+";";
+        return cliente.executaUpdate(sql);
     }
 
     protected boolean apaga(String nomeTabela) {
-        return true;
+        MySQL cliente = MySQL.getInstance();
+        String sql = "DELETE FROM "+nomeTabela+" WHERE id="+this.getID()+";";
+        return cliente.executaDelete(sql);
+    }
+    private String getMateriaSQL(){
+        if(this.materia == null){
+            return "NULL";
+        }
+        return this.getMateria().getID()+"";
+    }
+    private String getCriadorSQL(){
+        if(this.criador == null){
+            return "NULL";
+        }
+        return this.getCriador().getID()+"";
     }
 }
