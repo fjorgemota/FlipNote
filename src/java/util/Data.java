283839ccfa663313019cbfdf098ed873b65ce5f1
Fernando Jorge Mota <f.j.mota13@gmail.com>
2012-06-06 15:34:56 -0300

    Melhorias na classe de administracao de datas

--- src/java/util/Data.java
+++ src/java/util/Data.java
@@ -5,6 +5,7 @@
 package util;
 
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -15,16 +16,40 @@ import java.util.Date;
  */
 public class Data {
     public static String getDateTime() {
-        return getDateByFormat("yyyy/MM/dd HH:mm:ss");
+        return getDateByFormat("yyyy-MM-dd HH:mm:ss");
+    }
+    public static Date getDateTime(String s) {
+        return getDateFromString(s, "yyyy-MM-dd HH:mm:ss");
+    }
+    public static String getDateTime(Date data) {
+        return getStringByFormat(data, "yyyy-MM-dd HH:mm:ss");
     }
     public static String getDate() {
-        return getDateByFormat("yyyy/MM/dd");
+        return getDateByFormat("yyyy-MM-dd");
+    }
+    public static String getDate(Date data) {
+        return getStringByFormat(data, "yyyy-MM-dd");
+    }
+    public static Date getDate(String s) {
+        return getDateFromString(s, "yyyy-MM-dd");
     }
     public static String getDateTime(int plus) {
-        return getDateByFormat("yyyy/MM/dd HH:mm:ss", plus);
+        return getDateByFormat("yyyy-MM-dd HH:mm:ss", plus);
+    }
+    public static Date getDateTime(String s, int plus) {
+        return getDateFromString(s, "yyyy-MM-dd HH:mm:ss", plus);
+    }
+    public static String getDateTime(Date data, int plus) {
+        return getStringByFormat(data, "yyyy-MM-dd HH:mm:ss", plus);
     }
     public static String getDate(int plus) {
-        return getDateByFormat("yyyy/MM/dd", plus);
+        return getDateByFormat("yyyy-MM-dd", plus);
+    }
+    public static Date getDate(String s, int plus) {
+        return getDateFromString(s, "yyyy-MM-dd", plus);
+    }
+    public static String getDate(Date data, int plus) {
+        return getStringByFormat(data, "yyyy-MM-dd", plus);
     }
     public static String getDateByFormat(String format) {
         DateFormat dateFormat = new SimpleDateFormat(format);
@@ -38,5 +63,36 @@ public class Data {
         Date date = calendario.getTime();
         return dateFormat.format(date);
     }
-    
+    public static String getStringByFormat(Date date, String format) {
+        DateFormat dateFormat = new SimpleDateFormat(format);
+        return dateFormat.format(date);
+    }
+    public static String getStringByFormat(Date date, String format, int plus) {
+        DateFormat dateFormat = new SimpleDateFormat(format);  
+        Calendar calendario = Calendar.getInstance();
+        calendario.setTime(date);
+        calendario.add(Calendar.SECOND, plus);
+        Date data = calendario.getTime();
+        return dateFormat.format(data);
+    }
+    public static Date getDateFromString(String s, String format){
+        DateFormat dateFormat = new SimpleDateFormat(format);  
+        try {
+            return dateFormat.parse(s);
+        } catch (ParseException ex) {
+            return null;
+        }
+    }
+    public static Date getDateFromString(String s, String format, int plus){
+        DateFormat dateFormat = new SimpleDateFormat(format);  
+        Calendar calendario = Calendar.getInstance();
+        try {
+            calendario.setTime(dateFormat.parse(s));
+        } catch (ParseException ex) {
+            return null;
+        }
+        calendario.add(Calendar.SECOND, plus);
+        Date date = calendario.getTime();
+        return date;
+    }
 }
